d3b23ae1098f60fc27ce1068258bd639c1bd62d8
Thiago Costa <thiago.costa@gtrconsultoria.com.br>
2025-03-20 14:38:00 -0300

    new: test ecr

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "rockeseat-ci-api" {
-  name = "rockeseat-ci"
+  name = "rocketseat-ci"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
